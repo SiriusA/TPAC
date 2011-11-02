a09702f7f2d9c39a6eaa0172b713a38be45a4033
Kyle Picinich <kyle.kjoseph@gmail.com>
2011-11-02 16:01:33 -0400

    Conversion program

---
+++ convert.cpp
@@ -0,0 +1,16 @@
+//use www.cplusplus.com as a reference
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main(){
+	//retrieve data
+	ifstream infile;
+	infile.open ("Calendar.txt, ifstream::in);
+	
+	
+	
+	infile.close
+	//begin conversion
+}
