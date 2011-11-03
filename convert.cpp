45406024f24ebc38453e16bccc6e93a4191e5c62
Kyle Picinich <kyle.kjoseph@gmail.com>
2011-11-03 07:28:14 -0400

    Conversion 0.01

--- convert.cpp
+++ convert.cpp
@@ -6,11 +6,33 @@ using namespace std;
 
 int main(){
 	//retrieve data
-	ifstream infile;
-	infile.open ("Calendar.txt, ifstream::in);
-	
-	
-	
-	infile.close
+	char Mv;
+	char Tv;
+	char Wv;
+	char Fv;
+	cout<<"Due to limitations in the program, please type the letters M,T,W, and F, pressing enter after each.";
+	cin>>Mv;
+	cin>>Tv;
+	cin>>Wv;
+	cin>>Fv;
+	char line1;
+
+	ifstream is;
+
+	is.open ("Calendar.txt");
+
+	while (is.good())
+	{
+		if(is.get()=Fv)
+		{line1= is.getline ("F",256);}
+		if(is.get()=Mv)
+		{line1= is.getline ("M",256);}
+
+		if (is.good())
+		cout<<line1;
+	}
+
+	is.close();
+	return 0;
 	//begin conversion
 }
