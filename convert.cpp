c97ce5f949567250c8de6a9334144be8748fa790
Kyle Picinich <kyle.kjoseph@gmail.com>
2011-11-03 12:57:43 -0400

    Conversion which works

--- convert.cpp
+++ convert.cpp
@@ -1,38 +1,43 @@
 //use www.cplusplus.com as a reference
 #include<iostream>
 #include<fstream>
+#include<string>
 
 using namespace std;
 
 int main(){
 	//retrieve data
-	char Mv;
-	char Tv;
-	char Wv;
-	char Fv;
-	cout<<"Due to limitations in the program, please type the letters M,T,W, and F, pressing enter after each.";
-	cin>>Mv;
-	cin>>Tv;
-	cin>>Wv;
-	cin>>Fv;
-	char line1;
+	char data;
+	char x;
 
 	ifstream is;
 
 	is.open ("Calendar.txt");
-
+		cout<<"<li>";
 	while (is.good())
 	{
-		if(is.get()=Fv)
-		{line1= is.getline ("F",256);}
-		if(is.get()=Mv)
-		{line1= is.getline ("M",256);}
-
+		data=is.get();
+		if (data== '\n')
+		{cout<<data<<"</li><li>";}
 		if (is.good())
-		cout<<line1;
+		{cout<<data;}
 	}
-
+		cout<<"</li>";
 	is.close();
+
+	is.open ("Calendar.txt");
+			cout<<"<li>";
+		while (is.good())
+		{
+			data=is.get();
+			if (data== '\n')
+			{cout<<data<<"</li><li>";}
+			if (is.good())
+			{cout<<data;}
+		}
+			cout<<"</li>";
+		is.close();
+
 	return 0;
 	//begin conversion
 }
