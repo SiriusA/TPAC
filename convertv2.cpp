cd016a103e89652ee08bb097b9ab36b7ac94466d
Kyle Picinich <kyle.kjoseph@gmail.com>
2011-11-04 15:52:33 -0400

    V2 Conversion WIP

---
+++ convertv2.cpp
@@ -0,0 +1,42 @@
+//use www.cplusplus.com as a reference
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+int main(){
+	//retrieve data
+	char data;
+	string line;
+	line="";
+	int x;
+	x=0;
+	ofstream of;
+	ifstream is;
+
+	is.open ("Calendar.txt");
+	of.open ("output.htm");
+		of.write("<html><body><ul><li>",20);
+	while (is.good())
+	{
+		data=is.get();
+		line=line + data;
+		if (data== '\n')
+		{
+		cout<<line;
+		line="";
+		of.put(data);
+		of.write("</li><li>",9);
+		}
+		if (is.good())
+		{
+		of.put(data);
+		}
+	}
+		of.write("</li></ul></body></html>",24);
+	is.close();
+	of.close();
+	cout<<"Process Complete!";
+	return 0;
+}
